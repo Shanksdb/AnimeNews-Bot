0c762b04b6bd02da7f1251a2bf2a7c8475f88d72
Shanksdb <akashmadness109@gmail.com>
2025-03-19 13:34:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 # config.py
 
-API_ID = "" #API ID get it from telegram.org
-API_HASH = "" #API HASH get it from telegram.org
+API_ID = "28713982" #API ID get it from telegram.org
+API_HASH = "237e15f7c006b10b4fa7c46fee7a5377" #API HASH get it from telegram.org
 BOT_TOKEN = "" #Bot Token get it from @BotFather
-URL_A = "" #News RSS Feed Url 2
+URL_A = "https://www.theanimedaily.com/feed/" #News RSS Feed Url 2
 #URL_B = "" #News RSS Feed Url 2
-START_PIC = "" #Startup Pic
-MONGO_URI = "" #Databse uri get it from https://www.mongodb.com/
+START_PIC = "https://telegra.ph/file/2afe8898ae5f6ced9e8f7.jpg" #Startup Pic
+MONGO_URI = "mongodb+srv://Mrdaxx123:Mrdaxx123@cluster0.q1da65h.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0" #Databse uri get it from https://www.mongodb.com/
